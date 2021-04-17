57fd3f2ec630f952b15c0c808e8f1a225468dc4d
Ahmed Zahran <a.ahzahran@dev.thiqah.sa>
2021-04-17 16:34:11 +0300

    Second

---
+++ src/Architecture.DDD.EntityFrameworkCore/Infra/DeviceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Architecture.DDD.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Architecture.DDD.Infra
+{
+    public class DeviceRepository : EfCoreRepository<DDDDbContext, Device, Guid>
+    {
+        public DeviceRepository(IDbContextProvider<DDDDbContext> dbContextProvider): base(dbContextProvider)
+        {
+
+        }
+
+        public Task<Device> FindAsync(Guid id, string name)
+        {
+            return base.FindAsync(x => x.Id == id && x.Name == name);
+        }
+    }
+}
