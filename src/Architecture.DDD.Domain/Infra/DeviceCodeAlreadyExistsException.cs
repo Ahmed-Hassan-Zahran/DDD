c942e5526efa2f33fae224c67f63541008ddb381
Ahmed Zahran <ahzahran@thiqah.sa>
2021-04-21 15:51:26 +0300

    Change the Exception yo user friendlt

--- src/Architecture.DDD.Domain/Infra/DeviceCodeAlreadyExistsException.cs
+++ src/Architecture.DDD.Domain/Infra/DeviceCodeAlreadyExistsException.cs
@@ -2,7 +2,7 @@
 
 namespace Architecture.DDD.Infra
 {
-    public class DeviceCodeAlreadyExistsException : BusinessException
+    public class DeviceCodeAlreadyExistsException : UserFriendlyException
     {
         public DeviceCodeAlreadyExistsException(string code) : base("DCE:0000001", $"A device with code {code} already exists.")
         {
